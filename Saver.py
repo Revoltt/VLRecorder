d458e72840810b009a4da981dd00789b02034e63
Revoltt <zinchenk1994@gmail.com>
2016-06-18 19:58:11 +0300

    Correct lecture switching after 25

--- Saver.py
+++ Saver.py
@@ -1,21 +1,22 @@
 import win32api
 import win32con
-from sys import exit
 from time import sleep
 from PIL import ImageGrab
 
 print "All coordinates for mouse clicks are set for screen size 1366x768 pixels"
-print "Automatic recording starts in 10 seconds. Please switch to the window you want to record"
-
-#keyboard codes
+print "Automatic recording starts in 7 seconds. Please switch to the window you want to record"
+    
+# keyboard codes
 start = 0xBB # =
 close_1 = 0xA2 # left Ctrl
 close_2 = 0x57 # w
-sleep(10)
-
-#main lecture loop
+sleep(7)
+# variable for correct lecture switching after lecture 25
+k = 0
+    
+# main lecture loop
 while True:
-    #left click on the lecture
+    # left click on the lecture
     x = 640
     y = 385
     win32api.SetCursorPos((x,y))
@@ -23,11 +24,21 @@ while True:
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
     sleep(4.5)
-    
-    # start recording
-    win32api.keybd_event(start,0,0,0) # holds the "=" key down
-    win32api.keybd_event(start,0,win32con.KEYEVENTF_KEYUP,0)
-        
+           
+    # the player can be placed not in the usual spot. then we need to press one more button
+    # This check is BAD
+    x = 480
+    y = 100
+    img = ImageGrab.grab()
+    pixel = img.getpixel((x, y))
+    if pixel == (43, 187, 134):
+        x = 905
+        y = 125
+        win32api.SetCursorPos((x,y))
+        sleep(0.5)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+         
     # press play button in the player
     x = 440
     y = 400
@@ -36,10 +47,10 @@ while True:
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
     sleep(2.5)
-    #second time? not quite understood why it is needed
+    # second time? not quite understood why it is needed
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-    
+        
     # make sure the recording starts from the beginning of the lecture
     # for some reason this pauses the player
     x = 3
@@ -48,7 +59,7 @@ while True:
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
     sleep(1.5)
-    
+        
     # full screen
     x = 870
     y = 680
@@ -57,6 +68,12 @@ while True:
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
     
+    # start recording
+    sleep(0.5)
+    win32api.keybd_event(start,0,0,0) # holds the "=" key down
+    win32api.keybd_event(start,0,win32con.KEYEVENTF_KEYUP,0)
+    sleep(0.5)
+    
     # start the player again
     x = 440
     y = 400
@@ -64,7 +81,7 @@ while True:
     sleep(0.5)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-    
+        
     # wait till the lecture is nearly over
     sleep(5400) # average lecture length - 1.5 hours
     end =  False # lecture finish flag
@@ -72,46 +89,47 @@ while True:
     while True:
         img = ImageGrab.grab()
         #check certain image pixels
-        
+          
         x = 100
         y = 100
         pixel = img.getpixel((x, y))
         #print pixel
         end = (pixel[0] == 244) & (pixel[1] == 99) & (pixel[2] == 33)
-        
+          
         x = 1300
         y = 650
         pixel = img.getpixel((x, y))
         #print pixel
         end = end & (pixel[0] == 244) & (pixel[1] == 99) & (pixel[2] == 33)
-        
+              
         x = 1300
         y = 600
         pixel = img.getpixel((x, y))
         #print pixel
         end = end & (pixel[0] == 244) & (pixel[1] == 99) & (pixel[2] == 33)
-        
+          
         if end:
             print "done"
             break
         else:
             print "not yet"
-            sleep(120)
+            sleep(180)
     # stop the recording
+    sleep(0.5)
     win32api.keybd_event(start,0,0,0) # holds the "=" key down
     win32api.keybd_event(start,0,win32con.KEYEVENTF_KEYUP,0)
-    
+    sleep(0.5)
+     
     # close the tab (for google chrome)
     win32api.keybd_event(close_1,0,0,0) # holds the "left-Ctrl" key down
     win32api.keybd_event(close_2,0,0,0) # holds the "w" key down
     sleep(0.05)
     win32api.keybd_event(close_1,0,win32con.KEYEVENTF_KEYUP,0)
     win32api.keybd_event(close_2,0,win32con.KEYEVENTF_KEYUP,0)
-    
+    sleep(2)
     #switch to the next lecture
     x = 420
     y = 300
-    k = 0
     img = ImageGrab.grab()
     if img.getpixel((x, y)) == (231, 238, 241):
         #option 1 - lectures up to 24
@@ -121,9 +139,9 @@ while True:
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
     else:
-        #option 2 - lectures 24-30
+        #option 2 - lectures 25-30
         k = k + 1
-        y = y + 70 * k - 15
+        y = y + 65 * (k + 1) - 15
         win32api.SetCursorPos((x,y))
         sleep(0.5)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
